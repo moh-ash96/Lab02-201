f54f9055af4f35d9c028a639dcdc01c518ea8c25
mohammad ashour <moh.ash96@gmail.com>
2021-02-09 21:36:18 +0200

    js and css modification

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+let score = 0;
 let userName = prompt('What is your name?');
 // console.log(userName);
 alert ('Hello '+ userName + '! ' + 'Let\'s paly a game' );
@@ -13,16 +13,20 @@ switch(yourName.toUpperCase()){
     case'YES':
         // console.log(truth);
         alert(truth);
+        score++;
         break;
     case'Y':
         // console.log(truth);
         alert(truth);
+        score++;
+        // console.log(score);
         break;
     case'YEAH':
         // console.log(truth);
         alert(truth);
+        score++;
         break;
-    
+
     default:
         // console.log(lie);
         alert(lie);
@@ -34,16 +38,19 @@ switch(internet.toUpperCase()){
     case'YES':
         // console.log(truth);
         alert(truth);
+        score++;
         break;
     case'Y':
         // console.log(truth);
         alert(truth);
+        score++;
         break;
     case'YEAH':
         // console.log(truth);
         alert(truth);
+        score++;
         break;
-    
+
     default:
         // console.log(lie);
         alert(lie);
@@ -56,14 +63,16 @@ switch(disappear.toUpperCase()){
     case 'NO':
         // console.log(truth);
         alert(truth);
+        score++;
         break;
     case 'N':
         // console.log(truth);
         alert(truth);
+        score++;
         break;
     default:
         // console.log('I guess you watch alot of sci-fi movies!');
-        alert('I guess you watch alot of sci-fi movies!');            
+        alert('I guess you watch a lot of sci-fi movies!');            
 }
 
 let wrongDay = 'You have to go back to primary school!';
@@ -81,10 +90,13 @@ switch(day.toUpperCase()){
     case 'NO':
         // console.log(truth);
         alert(truth);
+        score++;
         break;
+       
     case 'N':
         // console.log(truth);
         alert(truth);
+        score++;
         break;
     default:
         // console.log('You are not taking me seriously!');
@@ -96,12 +108,60 @@ switch(read.toUpperCase()){
     case 'YES':
         // console.log('Good for you');
         alert('Good for you 😝');
+        score++;
         break;
     case 'Y':
         // console.log('Good for you');
         alert('Good for you 😝');
+        score++;
         break;
     default:
         // console.log('Lier🤨');
         alert('Lier 🤨');
 }
+
+let userNum= prompt('I have a number in my mind, I\'m expecting you to guess it, you have 4 attempts to get the right answer, What is that number?');
+for (let i = 0; i < 3; i++) {
+
+    if (userNum < 3) {
+        userNum=prompt('The number is more than that');
+    }else if(userNum > 3){
+        userNum=prompt('The number is less than that');
+    }else if(userNum == 3){
+        alert('correct');
+        score++;
+        break;
+    }
+ 
+}
+alert('The correct answer is 3');
+
+let food = ['chinese', 'italian', 'arabian', 'american'];
+let yup = false;
+
+for (let i = 0; i < 6; i++) {
+    let fav = prompt('Guess one of my favorite kitchens?');
+    console.log('in the first loop' + i);
+
+    for (let j = 0; j < food.length; j++) {
+        console.log('in the second loop' + j);
+        if (fav == food[j]) {
+            alert('Correct answer!');
+            yup= true;
+            score++;
+            break;
+        }
+
+    }
+    if(yup == true){
+        break;
+    }else{
+        alert('Wrong answer')
+        
+    }
+    
+
+
+}
+alert('the correct answers are '+ food)
+alert('Your score is ' + score);
